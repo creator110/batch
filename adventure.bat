58c1d5755c60e8aeee677388a04a5859089652a9
creator110 <creator110@gmx.de>
2015-04-09 13:53:37 +0200

    Create adventure.bat

---
+++ adventure.bat
@@ -0,0 +1,38 @@
+:#########################################################################################
+:# published under the x-license 														                             #
+:# Copyright (c) 2015 http://www.creator110.com 										                     #
+:#########################################################################################
+
+:stellt den Echoprompt ab
+@echo off
+
+:setzt die Farbe vom Hintergrund(0) und der Schrifft(2)
+color 02
+
+echo ####### ####### ####### ####### ####### ####### #######   ###    ###  ########
+echo #       #     # #       #     #    #    #     # #     # #####  #####  ##    ##
+echo #       #  ###  #####   #######    #    #     # #  ###    ###    ###  ##    ##
+echo #       #    #  #       #     #    #    #     # #    #    ###    ###  ##    ##
+echo ####### #     # ####### #     #    #    ####### #     #   ###    ###  ########
+echo                                  THE ADVENTURE
+pause
+
+:eingabe 
+set /p user=Wer bist du?
+goto ausgabe
+
+:eingabe2
+set /p user=Wer bist du dann?
+goto ausgabe
+
+:nameKorrektur 
+set /P ANTW=Ist dein Name wirklich %user%? (j/n)   
+echo user: %user%>conf.txt
+if %ANTW%==j goto eingabe
+if %ANTW%==n goto eingabe2
+
+:nameJa
+echo Na dann mal los %user%;)
+
+:end
+pause
